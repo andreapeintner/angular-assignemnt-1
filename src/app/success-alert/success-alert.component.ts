d11945183b3f0136792fc9fc967944f671ae6d16
Andrea Peintner <andreapeintner@gmail.com>
2018-12-10 10:40:54 +0100

    conclude assignment

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styles: [`
+    h4 {
+      color: green;
+    }
+  `]
+})
+
+export class SuccessAlertComponent {
+
+}
